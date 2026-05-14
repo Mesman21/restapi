b5da5c34e408bddcb4d966ce2836986b7241037a
Mesman21 <149572864+Mesman21@users.noreply.github.com>
2026-05-14 19:22:13 +0300

    lab1

--- schemas/book.py
+++ schemas/book.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from enum import Enum
 from typing import Optional
 
@@ -23,6 +23,4 @@ class BookCreate(BookBase):
 class BookResponse(BookBase):
     id: str
 
-    class Config:
-        orm_mode = True
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
